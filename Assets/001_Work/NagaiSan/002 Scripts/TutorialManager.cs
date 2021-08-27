d0815db5920f7a6e172189db5bebfc570efde85d
MichiakiNagai <nagai.michiaki.xr@gmail.com>
2021-08-27 13:38:00 +0900

    MN - Add Tutorial Messages on Stage0
    Add Tutorial Messages - Completed-
    Add Tutorial a Video - Interact with Objects
    
    Need to Add Media about Tutorials.

--- Assets/001_Work/NagaiSan/002 Scripts/TutorialManager.cs
+++ Assets/001_Work/NagaiSan/002 Scripts/TutorialManager.cs
@@ -6,14 +6,16 @@ using UnityEngine.SceneManagement;
 public class TutorialManager : MonoBehaviour
 {
     #region Require Values
+    public GameObject[] tutorialObjects = new GameObject[3];
+
     #region UIs
     // Texts
     //public List<GameObject> guideTexts;
-    public GameObject[] guideTexts = new GameObject[18];
+    public GameObject[] guideTexts = new GameObject[34];
 
     // Images
     //public List<GameObject> guideImages;
-    public GameObject[] guideImages = new GameObject[2];
+    public GameObject[] guideImages = new GameObject[8];
     #endregion // UIs
 
     private int readNum = 0;
@@ -35,6 +37,10 @@ public class TutorialManager : MonoBehaviour
             guideTexts = new List<GameObject>();
             guideImages = new List<GameObject>();
             */
+            for (int i = 0; i < tutorialObjects.Length; i++)
+            {
+                tutorialObjects[i].SetActive(false);
+            }
             for (int i = 0; i < guideTexts.Length; i++)
             {
                 guideTexts[i].SetActive(false);
@@ -44,7 +50,13 @@ public class TutorialManager : MonoBehaviour
             {
                 guideImages[i].SetActive(false);
             }
-            
+
+            GameObject pauseMenu = GameObject.Find("PauseMenu");
+            if (pauseMenu)
+            {
+                pauseMenu.SetActive(false);
+            }
+
             readNum = 0;
         }
     }
@@ -55,12 +67,14 @@ public class TutorialManager : MonoBehaviour
 
         switch (readNum)
         {
+            #region case 0
             case 0:
                 guideImages[0].SetActive(true);
                 guideTexts[readNum].SetActive(true);
                 readDone = true;
                 break;
-
+            #endregion // case 0
+            #region case 1~34
             case 1:
             case 2:
             case 3:
@@ -79,8 +93,26 @@ public class TutorialManager : MonoBehaviour
             case 16:
             case 17:
             case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
                 if (OVRInput.GetDown(OVRInput.RawButton.A) && !readDone)
                 {
+                    
+
                     #region Display Images
                     if (readNum == 1){}
                     else if(1 < readNum && readNum <= 8)
@@ -108,18 +140,53 @@ public class TutorialManager : MonoBehaviour
                         guideImages[4].SetActive(false);
                         guideImages[5].SetActive(true);
                     }
-                    else if (15 < readNum && readNum <= 18)
+                    else if (15 < readNum && readNum <= 17)
                     {
                         guideImages[5].SetActive(false);
                         guideImages[6].SetActive(true);
                     }
+                    else if (17 < readNum && readNum <= 27)
+                    {
+                        guideImages[6].SetActive(false);
+                        guideImages[7].SetActive(true);
+                    }
                     #endregion // Display Images
+                    else if (readNum == 26)
+                    {
+                        for (int i = 0; i < tutorialObjects.Length -1; i++)
+                        {
+                            tutorialObjects[i].SetActive(true);
+                        }
+                    }
 
                     guideTexts[readNum -1].SetActive(false);
-                    guideTexts[readNum].SetActive(true);
+
+                    if (0 < readNum && readNum < 34)
+                    {
+                        guideTexts[readNum].SetActive(true);
+                    }
+                    
                     readDone = true;
+
+                    if (readDone && readNum == 32)
+                    {
+                        endTutorialFlag = true;
+                    }
+
+                    if (readDone && readNum == 34)
+                    {
+                        GameObject messages = GameObject.Find("Messages");
+                        messages.SetActive(false);
+
+                        readNum = -1;
+                        readDone = false;
+
+                        tutorialObjects[2].SetActive(true);
+                    }
                 }
+                
                 break;
+            #endregion // case 1~34
 
             default:
                 break;
